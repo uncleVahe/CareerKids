9f7b2ddebcca301ca7ad3561c8c5de9f6455585e
sme-bazi001 <vahe.bazikian.intimetechnologies@sonymusic.com>
2025-12-19 17:07:51 +0000

    Initial Commit

---
+++ CareerKids/CareerKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerKidsApp.swift
+//  CareerKids
+//
+//  Created by Vahe Bazikyan on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CareerKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
